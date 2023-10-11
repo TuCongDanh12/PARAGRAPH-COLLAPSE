2f899b9ed44b0b79e30bc880e66c8f7f48832c84
TuCongDanh12 <tucondanh9a1@gmail.com>
2023-10-12 01:35:39 +0700

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,56 @@
-var coll = document.querySelectorAll(".collapsible");
+const coll = document.querySelectorAll(".vocab .collapsible");
 
 // console.log(coll)
 coll.forEach((coll) => {
-  coll.addEventListener("click", ()=>{
-    coll.classList.add("active")
-    showhide=coll.nextElementSibling;
-    if (showhide.style.display === "block") {
-        showhide.style.display = "none";
-      } else {
-        showhide.style.display = "block";
-      }
+  coll.addEventListener("click", () => {
+    // coll.classList.toggle("active")
+    var showhide = coll.nextElementSibling;
+    // console.log(showhide)
+    if (showhide.style.maxHeight) {
+      // console.log(showhide.style.maxHeight)
+      showhide.style.maxHeight = null;
+    } else {
+      // console.log(showhide.style.maxHeight)
+      showhide.style.maxHeight = showhide.scrollHeight + "px";
+    }
   });
 });
 
+const showhide = (acc) => {
+  const showhide = acc.nextElementSibling;
+  if (showhide.style.maxHeight) {
+    showhide.style.maxHeight = null;
+  } else {
+    showhide.style.maxHeight = showhide.scrollHeight + "px";
+  }
+};
 
+const hide = (acc) => {
+  const showhide = acc.nextElementSibling;
+  showhide.style.maxHeight = null;
+};
+
+const accordion = document.querySelectorAll(".accordion .collapsible");
+console.log(accordion);
+
+accordion.forEach((acc, index) => {
+  acc.addEventListener("click", () => {
+    accordion.forEach((acc, id) => {
+      if (index === id) {
+        showhide(accordion[index]);
+      } else {
+        hide(accordion[id]);
+      }
+    });
+  });
+});
+
+const collapsible=document.querySelectorAll(".collapsible");
+const icon = document.querySelectorAll("i");
+// console.log(icon);
+icon.forEach((icon,index) => {
+  collapsible[index].addEventListener("click", () => {
+    icon.classList.toggle("fa-caret-down")
+    icon.classList.toggle("fa-caret-left")
+  });
+});
